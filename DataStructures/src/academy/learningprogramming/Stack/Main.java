42e5d3f7d872548040c18a767a0676a3bfba0ee0
iantsai <ian01045@gmail.com>
2023-07-25 09:31:41 +0800

    jdk linked list impls stack sample

--- DataStructures/src/academy/learningprogramming/Stack/Main.java
+++ DataStructures/src/academy/learningprogramming/Stack/Main.java
@@ -12,21 +12,37 @@ public class Main {
         Employee johnDoe = new Employee("John","Doe",4567);
         Employee marySmith = new Employee("Marry","Smith",22);
         Employee mikeWilson = new Employee("Mike","Wilson",3245);
+        Employee billEnd = new Employee("Bill","End",78);
 
-        ArrayStack stack = new ArrayStack(10);
+        //array stack implementation
+//        ArrayStack stack = new ArrayStack(10);
+//        stack.push(janeJones);
+//        stack.push(johnDoe);
+//        stack.push(marySmith);
+//        stack.push(mikeWilson);
+//        stack.push(billEnd);
+//        //stack.printStack();
+//
+//        System.out.println(stack.peek());
+//
+//        System.out.println("Popped: " + stack.pop());
+//
+//        System.out.println(stack.peek());
+
+        //linked stack implementation
+        LinkedStack stack = new LinkedStack();
         stack.push(janeJones);
         stack.push(johnDoe);
         stack.push(marySmith);
         stack.push(mikeWilson);
-        stack.push(new Employee("Bill","End",78));
+        stack.push(billEnd);
 
-        //stack.printStack();
+//        stack.printStack();
+//        System.out.println(stack.peek());
+//        stack.printStack();
 
-        System.out.println(stack.peek());
-
-        System.out.println("Popped: " + stack.pop());
-
-        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        stack.printStack();
 
     }
 
