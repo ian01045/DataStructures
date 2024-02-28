fea9a2d3535e33a7cfe0da695d597f2875c61098
iantsai <ian01045@gmail.com>
2024-02-29 02:24:01 +0800

    binary search tree - get,min,max method

--- DataStructures/src/academy/learningprogramming/BinarySearchTree/Main.java
+++ DataStructures/src/academy/learningprogramming/BinarySearchTree/Main.java
@@ -14,8 +14,14 @@ public class Main {
         intTree.insert(22);
         intTree.insert(32);
 
-
         intTree.traverseInOrder();
 
+        System.out.println();
+        System.out.println(intTree.get(27).getData());
+        System.out.println(intTree.get(15).getData());
+        System.out.println(intTree.get(8888));
+
+        System.out.println(intTree.min());
+        System.out.println(intTree.max());
     }
 }
--- DataStructures/src/academy/learningprogramming/BinarySearchTree/TreeNode.java
+++ DataStructures/src/academy/learningprogramming/BinarySearchTree/TreeNode.java
@@ -5,7 +5,9 @@ public class TreeNode {
     private int data;
     private TreeNode leftChild;
     private TreeNode rightChild;
-
+    public TreeNode(int data){
+        this.data = data;
+    }
     public void insert( int value ){
         if(value == data){
             return;
@@ -26,6 +28,40 @@ public class TreeNode {
         }
     }
 
+    public TreeNode get (int value ){
+        if( value == data){
+            return this;
+        }
+
+        if( value < data ){
+            if( leftChild != null){
+                return leftChild.get(value);
+            }
+        }else{
+            if( rightChild != null){
+                return rightChild.get(value);
+            }
+        }
+
+        return null;
+    }
+
+    public int min(){
+        if(leftChild==null){
+            return data;
+        }else{
+            return leftChild.min();
+        }
+    }
+
+    public int max(){
+        if(rightChild==null){
+            return data;
+        }else{
+            return rightChild.max();
+        }
+    }
+
     public void traverseInOrder(){
         if(leftChild != null){
             leftChild.traverseInOrder();
@@ -36,9 +72,7 @@ public class TreeNode {
         }
     }
 
-    public TreeNode(int data){
-        this.data = data;
-    }
+
 
     public int getData() {
         return data;
